dce8bcbe021cac20a4b3fb095b587472f88563c0
doha <jjeenee0408@gmail.com>
2025-05-20 17:13:15 +0900

    회원 데이터

--- src/main/java/springsecuritytask/model/dto/UserDTO.java
+++ src/main/java/springsecuritytask/model/dto/UserDTO.java
@@ -1,4 +1,30 @@
 package springsecuritytask.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import springsecuritytask.model.entity.UserEntity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDTO {
+    private  int uno;
+    private String username;
+    private  String useremail;
+    private  String userpassword;
+    private String role;
+
+    public UserEntity toEntity (){
+        return UserEntity.builder()
+                .uno(this.uno)
+                .username(this.username)
+                .useremail(this.useremail)
+                .userpassword(this.userpassword)
+                .role(this.role)
+                .build();
+    }
+
 }
--- src/main/java/springsecuritytask/model/entity/UserEntity.java
+++ src/main/java/springsecuritytask/model/entity/UserEntity.java
@@ -1,4 +1,37 @@
 package springsecuritytask.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import springsecuritytask.model.dto.UserDTO;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int uno;
+
+    private String username;
+    @Column(unique = true)
+    private  String useremail;
+    private  String userpassword;
+    private String role;
+
+    public UserDTO toDTO(){
+        return UserDTO.builder()
+                .username(this.username)
+                .useremail(this.useremail)
+                .userpassword(this.userpassword)
+                .role(this.role)
+                .build();
+    }
+
 }
