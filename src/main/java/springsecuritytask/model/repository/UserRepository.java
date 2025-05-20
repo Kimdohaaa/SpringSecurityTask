10286886cee1114e3dab982a34ab8b2cf6c095ab
doha <jjeenee0408@gmail.com>
2025-05-20 17:14:11 +0900

    DB 연동(JPA)

--- src/main/java/springsecuritytask/model/repository/UserRepository.java
+++ src/main/java/springsecuritytask/model/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package springsecuritytask.model.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import springsecuritytask.model.entity.UserEntity;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    // 중복 이메일 확인
+    boolean existsByUseremail(String useremail);
+
+    //
+    UserEntity findByUseremail(String useremail);
 }
