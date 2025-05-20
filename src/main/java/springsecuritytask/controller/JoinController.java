86594dc4c7365bd7b026b4cbfb7c62d10f6bd6ce
doha <jjeenee0408@gmail.com>
2025-05-20 17:13:32 +0900

    회원가입

--- src/main/java/springsecuritytask/controller/JoinController.java
+++ src/main/java/springsecuritytask/controller/JoinController.java
@@ -2,11 +2,36 @@ package springsecuritytask.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import springsecuritytask.model.dto.UserDTO;
 import springsecuritytask.service.JoinService;
 
 @Controller
 @RequiredArgsConstructor
 public class JoinController {
     private final JoinService joinService;
+
+
+    @GetMapping("/join")
+    public String joinP(){
+        return "join"; // 회원가입 페이지 반환
+    }
+
+    // 회원가입
+    @PostMapping("/joinProc")
+    public String joinProcess(UserDTO userDTO){
+            //  JSON이 아니라 application/x-www-form-urlencoded 형식으로 받기 때문에 @RequestBody 사용 X
+        System.out.println("JoinController.joinProcess");
+        System.out.println("userDTO = " + userDTO);
+
+
+        boolean result = joinService.joinProcess(userDTO);
+
+        System.out.println(">>> 회원가입 결과 : " + result);
+
+        return  "redirect:/login"; // 로그인페이지로 리다이렉팅
+    }
 
 }
--- src/main/java/springsecuritytask/service/JoinService.java
+++ src/main/java/springsecuritytask/service/JoinService.java
@@ -1,12 +1,44 @@
 package springsecuritytask.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import springsecuritytask.model.dto.UserDTO;
+import springsecuritytask.model.entity.UserEntity;
 import springsecuritytask.model.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class JoinService {
     private  final UserRepository userRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    // 회원가입
+    public boolean joinProcess(UserDTO userDTO){
+        System.out.println("JoinService.joinProcess");
+        System.out.println("userDTO = " + userDTO);
+
+        // 중복 이메일 확인
+        userRepository.existsByUseremail(userDTO.getUseremail());
+
+        // 비밀번호 암호화
+        userDTO.setUserpassword(bCryptPasswordEncoder.encode(userDTO.getUserpassword()));
+
+        // 권한부여
+        userDTO.setRole("ROLE_ADMIN"); // 임시로 관리자 권한 부여
+
+        // DTO -> Entity
+        UserEntity userEntity = userDTO.toEntity();
+
+        UserEntity saveEntity = userRepository.save(userEntity);
+
+        if(saveEntity.getUno() > 0){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
 
 }
