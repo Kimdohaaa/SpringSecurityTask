255f8311376b26b628e52046730ce46b35b7c183
doha <jjeenee0408@gmail.com>
2025-05-20 17:12:57 +0900

    SpringSecurity 인가,검증 커스텀 클래스

---
+++ src/main/java/springsecuritytask/config/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package springsecuritytask.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig { // SpringSecurity 인가, 검증 을 커스텀하는 클래스
+
+    // [1] 비밀번호 암호화 메소드
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    // [2] SpringSecurity 인가, 검증 커스텀
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        
+        // 각 경로에 대한 설정
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/login", "/loginProc", "/join", "/joinProc").permitAll() // 모든 사용자 접근 허용
+                .requestMatchers("/admin").hasRole("ADMIN") // "ADMIN" 롤을 가진 사용자만 접근 가능
+                .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER") 
+                .anyRequest().authenticated() // 위에 지정한 경로에 해당되지 않는 경로에 대한 처리
+        );
+        
+        // 로그인 페이지 설정
+        http.formLogin((auth) -> auth.loginPage("/login")
+                .loginProcessingUrl("/loginProc")// 로그인 HTML 파일을 통해 사용자에게 입력 받은 정보를 지정한 경로로 보냄
+                .permitAll()
+        );
+        
+        // 토큰 임시로 끄기
+        http.csrf((auth) -> auth.disable());
+        
+        return http.build();
+    }
+    
+}
