cbcb13c8931a0662f0e88c56d1bcd084d162c0c5
doha <jjeenee0408@gmail.com>
2025-05-24 15:25:05 +0900

    UserDetails 인터페이스 구현 클래스

--- src/main/java/springsecuritytask/model/dto/CustomUserDetails.java
+++ src/main/java/springsecuritytask/model/dto/CustomUserDetails.java
@@ -7,6 +7,12 @@ import springsecuritytask.model.entity.UserEntity;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/*
+- CustomUserDetails 클래스는 UserDetails 인터페이스를 구현함
+- DB 에서 조회한 사용자 정보를 SpringSecurity 를 통해 인증, 권한 체크 하기 위해 사용
+- username/pasword/authorities/계정상태 오버라이딩 필수
+- 로그인 성공 후 CustomUserDetails 객체를 통해 SpringSecurity 에서 로그인된 사용자의 저보를 가져올 수 있음
+*/
 public class CustomUserDetails implements UserDetails {
 
     // 데이터를 받을 필드 선언
@@ -72,4 +78,27 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    // 추가로 사용할 데이터
+
+    // [1] Entity
+    public UserEntity getUserEntity() {
+        return this.userEntity;
+    }
+
+    // [2] 이메일
+    public String getUseremail() {
+        return userEntity.getUseremail();
+    }
+
+    // [3] 이름
+    public String getUsernameReal() {
+        return userEntity.getUsername();
+    }
+
+    // [4] Role
+    public String getRole() {
+        return userEntity.getRole();
+    }
+
 }
