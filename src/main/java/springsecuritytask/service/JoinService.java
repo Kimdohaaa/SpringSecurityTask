bca56d1d1c94d5402278d97eaa2aa372e3a01975
doha <jjeenee0408@gmail.com>
2025-05-24 15:25:44 +0900

    회원가입 시 유효성 검사 추가

--- src/main/java/springsecuritytask/service/JoinService.java
+++ src/main/java/springsecuritytask/service/JoinService.java
@@ -8,6 +8,8 @@ import springsecuritytask.model.dto.UserDTO;
 import springsecuritytask.model.entity.UserEntity;
 import springsecuritytask.model.repository.UserRepository;
 
+import java.util.regex.Pattern;
+
 @Service
 @RequiredArgsConstructor
 public class JoinService {
@@ -18,15 +20,35 @@ public class JoinService {
     public boolean joinProcess(UserDTO userDTO){
         System.out.println("JoinService.joinProcess");
         System.out.println("userDTO = " + userDTO);
-
-        // 중복 이메일 확인
-        userRepository.existsByUseremail(userDTO.getUseremail());
+        // 유효성 검사 //
+        // [1] 중복 이메일 확인
+        if(userRepository.existsByUseremail(userDTO.getUseremail())){
+            System.out.println(">>> 이메일 중복");
+            return false;
+        };
+        // [2] 이메일 길이
+        if(userDTO.getUseremail().length() < 5 || userDTO.getUseremail().length() > 17){
+            System.out.println(">>> 이메일 길이 오류 ");
+            return false ;
+        }
+        // [2] 이메일 형식 검사
+        String emailCheck = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        if(!Pattern.matches(emailCheck, userDTO.getUseremail())){
+            System.out.println(">>> 이메일 형식 오류 ");
+            return  false;
+        }
+        // [3] 비밀번호 복잡도 검사
+        String passwordCheck = "^(?=.*\\d)[A-Za-z\\d]{5,20}$";
+        if(!Pattern.matches(passwordCheck, userDTO.getUserpassword())){
+            System.out.println(">>> 비밀번호 복잡도 오류 ");
+            return false;
+        }
 
         // 비밀번호 암호화
         userDTO.setUserpassword(bCryptPasswordEncoder.encode(userDTO.getUserpassword()));
 
         // 권한부여
-        userDTO.setRole("ROLE_ADMIN"); // 임시로 관리자 권한 부여
+        userDTO.setRole("ROLE_USER"); // 임시로 관리자 권한 부여
 
         // DTO -> Entity
         UserEntity userEntity = userDTO.toEntity();
